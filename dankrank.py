036a4fb17d582ede88646831cea091a54a6fd2db
Connor Zapfel <connorzapfel@gmail.com>
2017-10-07 22:07:44 -0400

    Images are size-normalized

--- dankrank.py
+++ dankrank.py
@@ -1,9 +1,14 @@
 from DankSucc import DankSucc
+from PIL import Image
 
 # Succ all the memes
-succr = DankSucc("dankmemes,memes", 2)
+succr = DankSucc("memes")
 succr.succ()
+succr.persist()
 
 # Normalize the images
 for d in succr.grab_danks():
-    print(d.score)
+    dank_dir = "danks/"
+    img = Image.open(dank_dir+d.filename)
+    img = img.resize((1024,1024))
+    img.save(dank_dir+d.filename, d.filetype)
