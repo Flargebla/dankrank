28acd5ae80f64ea4e3c370d8c1e8a8168633bfa0
Connor Zapfel <connorzapfel@gmail.com>
2017-10-08 04:26:16 -0400

    Added excepts to catch and remove potentially broken danks

--- dankrank.py
+++ dankrank.py
@@ -2,13 +2,20 @@ from DankSucc import DankSucc
 from PIL import Image
 
 # Succ all the memes
-succr = DankSucc("memes")
+succr = DankSucc("me_irl")
 succr.succ()
 succr.persist()
 
 # Normalize the images
 for d in succr.grab_danks():
     dank_dir = "danks/"
-    img = Image.open(dank_dir+d.filename)
-    img = img.resize((1024,1024))
-    img.save(dank_dir+d.filename, d.filetype)
+    try:
+        img = Image.open(dank_dir+d.filename)
+        img = img.resize((1024,1024))
+        img.save(dank_dir+d.filename, d.filetype)
+    except PermissionError:
+        print("PermissionError, Failed to open:",dank_dir+d.filename)
+        succr.kill_dank(d)
+    except OSError:
+        print("OSError, Invalid image format:",dank_dir+d.filename) 
+        succr.kill_dank(d)
