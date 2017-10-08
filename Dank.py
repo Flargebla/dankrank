4d83c5c5bd903c32fd7a417a834eca5a3933124d
Connor Zapfel <connorzapfel@gmail.com>
2017-10-08 03:41:45 -0400

    Fixed filetype not being actually assigned and added field for created_at timestamp

--- Dank.py
+++ Dank.py
@@ -9,23 +9,24 @@ class Dank:
     # score - The normalized score
     # subreddit - The subreddit where the Dank originated
     # filetype - The type of the image file
-    def __init__(self, url, upvotes, sub):
+    def __init__(self, url, upvotes, sub, created):
         logging.basicConfig(level=logging.INFO,
                             format="%(asctime)s %(levelname)s %(message)s",
                             filename="debugging.log",
                             filemode="w")
         self.logger = logging.getLogger("Dank")
         self.url = url
+        self.created_at = created
         self.filename = urlsplit(self.url)[2].split('/')[-1]
         self.updoots = upvotes
         self.subreddit = sub
-        self.score = self.__calculate_score()
+        self.score = self.__calculate_initial_score()
         self.filetype = self.__detect_filetype()
 
-    # Calculate the Dank score
-    def __calculate_score(self):
+    # Calculate the initial score
+    def __calculate_initial_score(self):
         return self.updoots / self.subreddit.subscribers
 
     # Determine the filetype
     def __detect_filetype(self):
-        self.filetype = self.filename.split(".")[-1].upper()
+        return self.filename.split(".")[-1].upper()
