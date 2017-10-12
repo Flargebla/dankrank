17a7fd94d557777ee3a88d07d38b59e8da26b919
Connor Zapfel <czapfel@stevens.edu>
2017-10-11 23:21:50 -0400

    Added web app strucutre

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, request, render_template
+import random, time
+
+app = Flask(__name__)
+
+@app.route("/")
+def main_page():
+    return render_template("index.html")
+
+@app.route("/process_dank", methods=["POST"])
+def process_dank():
+    image = request.files.get("dank","")
+    image.save("static/images/"+image.filename)
+    time.sleep(2)
+    return "<img src='static/images/{}'>".format(image.filename)
+
